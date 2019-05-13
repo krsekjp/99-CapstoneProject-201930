f4ea355bf137ad59ad92aa716f7b408eabedd3f9
murrayj1 <murrayj1@rose-hulman.edu>
2019-05-13 08:42:15 -0400

    Added buttons

--- src/m3_laptop_code.py
+++ src/m3_laptop_code.py
@@ -19,12 +19,27 @@ import m2_laptop_code as m2
 def get_my_frame(root, window, mqtt_sender):
     # Construct your frame:
     frame = ttk.Frame(window, padding=10, borderwidth=5, relief="ridge")
-    frame_label = ttk.Label(frame, text="PUT_YOUR_NAME_HERE")
+    frame_label = ttk.Label(frame, text="Jacob Murray")
     frame_label.grid()
-    # TODO 2: Put your name in the above.
+    # Done 2: Put your name in the above.
 
     # Add the rest of your GUI to your frame:
     # TODO: Put your GUI onto your frame (using sub-frames if you wish).
+    arm_up_button = ttk.Button(frame, text='Arm Up')
+    arm_up_button.grid()
+    #arm_up_button["command"] = lambda:
+
+    calibrate_arm_button = ttk.Button(frame, text='Calibrate Arm')
+    calibrate_arm_button.grid()
+    #calibrate_arm_button["command"] = lambda:
+
+    arm_to_button = ttk.Button(frame, text='Move Arm')
+    arm_to_button.grid()
+    #arm_to_button["command"] = lambda:
+
+    arm_down_button = ttk.Button(frame, text='Arm Down')
+    arm_down_button.grid()
+    #arm_up_button["command"] = lambda:
 
     # Return your frame:
     return frame
